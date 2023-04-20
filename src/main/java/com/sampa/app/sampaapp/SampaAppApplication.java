2525af79d665914dde64d7502d818e2a0b61d205
sskiragu <skiragu18@gmail.com>
2023-04-20 13:39:53 +0300

    Initial commit

---
+++ src/main/java/com/sampa/app/sampaapp/SampaAppApplication.java
@@ -0,0 +1,13 @@
+package com.sampa.app.sampaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sampa/app/sampaapp/SampaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampa.app.sampaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
