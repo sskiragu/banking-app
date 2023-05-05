0b1ffee360b02c4eeac1c159565f600275afa71b
sskiragu <skiragu18@gmail.com>
2023-05-05 11:19:33 +0300

    Customer password encryption

--- src/main/java/com/sampa/app/sampaapp/service/UserServiceImpl.java
+++ src/main/java/com/sampa/app/sampaapp/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sampa.app.sampaapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.sampa.app.sampaapp.model.User;
@@ -12,8 +13,13 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PasswordEncoder bCryptPasswordEncoder;
+	
 	@Override
 	public User createUser(User user) {
+		String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
 		return userRepository.save(user);
 	}
 
